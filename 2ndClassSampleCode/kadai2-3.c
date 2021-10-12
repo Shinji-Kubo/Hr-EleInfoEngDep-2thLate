4e235a6dbef206a37fa944d5eed496dbc27a04d6
Shinji-Kubo <s.2001kubo@outlook.jp>
2021-10-12 17:19:49 +0900

    2ndClass

--- 2ndClassSampleCode/kadai2-3.c
+++ 2ndClassSampleCode/kadai2-3.c
@@ -1,42 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define N 256
+#define N 32
 
 typedef struct {
 	int cnt;
 } Quantity;
 
+int genRand(int A, int B, int M, int X) {
+	return abs((A * X + B) % M);
+}
+
 main(void) {
-	int i;
-
-	FILE *fp;
-	char fname[] = "kadai2-3-1.txt";
-	char str[N];
-	double num;
+	int i, j;
+	
+	static int A = 13;
+	static int B = 2 * N + 1;
+	static int M = pow(2, N);
+	int X = 0;
+	double rand;
 	Quantity quantity[10];
-	int cnt = 0;
 
-	fp = fopen(fname, "r");
-	if (fp == NULL) {
-		printf("%s couldn't open.\n", fname);
-		return -1;
+	for(i=0; i<10; i++) {
+		quantity[i].cnt = 0;
 	}
 
-	while (fgets(str, N, fp) != NULL) {
-		num = atof(str);
-		printf("%d : %f\n", cnt, num);
+	 for (i=0; i<M; i++) {
 
-		for (i=0; i<10; i++) {
-			if (num >= 0.1*i && num < 0.1*i+1) {
-				quantity[i].cnt++;
+		X = genRand(A, B, M, X);
+		rand = 1.0 * X / M;
+		printf("%d : %f\n", i, rand);
+
+		for (j=0; j<10; j++) {
+			if (rand >= 0.1*j && rand < 0.1*(j+1)) {
+				quantity[j].cnt++;
 				break;
 			}
 		}
 
-		cnt++;
-	}
+		printf("------------------------------------------------\n");
+		for (j=0; j<10; j++) {
+			printf("%f : %d\n", j, quantity[i].cnt);
+		}
+		printf("------------------------------------------------\n");
 
-	fclose(fp);
+	}
 
 	for (i=0; i<10; i++) {
 		printf("%d : %d\n", i, quantity[i].cnt);
