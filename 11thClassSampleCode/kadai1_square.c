350e5293fe2f397d451b5ede793cc07c2f28daf3
Shinji Kubo <s.2001kubo@outlook.jp>
2022-01-05 08:55:10 +0900

    11thClass

---
+++ 11thClassSampleCode/kadai1_square.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define NY 128
+#define NX 128
+
+int main(void)
+{
+	int i, j, a, b, c, d;
+	FILE *fp;
+	char fn[] = "squareImg";
+	float img[NY][NX];
+	float att_tis_pix;
+	float att_fac;
+	float den;
+	float size;
+
+	att_fac = 0.2264;
+	den = 1.06;
+	size = 0.25;
+
+	att_tis_pix = att_fac * den * size;
+
+	a = 32;
+	b = 96;
+	c = 32;
+	d = 96;
+
+	for (i = 0; i < NY; i++)
+	{
+		for (j = 0; j < NX; j++)
+		{
+			img[i][j] = 0;
+		}
+	}
+
+	for (i = 0; i < NY; i++)
+	{
+		for (j = 0; j < NX; j++)
+		{
+			if (j > a && j < b && i > c && i < d)
+			{
+				img[i][j] = att_tis_pix;
+			}
+		}
+	}
+
+	fp = fopen(fn, "wb");
+	if (fp == NULL)
+	{
+		printf("File open error \n");
+		exit(1);
+	}
+
+	for (i = 0; i < NY; i++)
+	{
+		fwrite(img[i], NX, sizeof(float), fp);
+	}
+
+	fclose(fp);
+
+	return 0;
+}
