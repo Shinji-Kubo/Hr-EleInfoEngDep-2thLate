68158b4b896fd3f6c78ea1ecc3cf0ff914a9caef
Shinji Kubo <s.2001kubo@outlook.jp>
2022-01-05 20:54:33 +0900

    11thClass

--- 11thClassSampleCode/kadai2.c
+++ 11thClassSampleCode/kadai2.c
@@ -1,55 +1,43 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-#define NZ 128
 #define NY 128
 #define NX 128
 
 int main(void) {
-    int i, j, k;
+    int i, j;
     double left, right;
     FILE *fp;
     char fn[] = "kadai2Img";
     char img[NY][NX];
     float size;
-    float a, b;
 
     size = 0.25;
 
     fp = fopen(fn, "wb");
-    if (fp == NULL) {
-        printf("File open error\n");
+    if (fp==NULL) {
+        printf("File open error \n");
         exit(1);
     }
 
-    for (i=0; i<NZ; i++) {
-        // Initialize
-        for (j=0; j<NY; j++) {
-            for (k=0; k<NX; k++) {
-                img[j][k] = '0';
+    for (i=0; i<NY; i++) {
+        for (j=0; j<NX; j++) {
+            img[i][j] = '0';
+        }
+    }
+
+    for (i=0; i<NY; i++) {
+        for (j=0; j<NX; j++) {
+            left  = pow(j-64, 2) / pow(28/size, 2);
+            right = pow(i-64, 2) / pow(20/size, 2);
+            if (left + right <= 1.0) {
+                img[i][j] = '120';
             }
         }
+    }
 
-        // Trunk
-        if (i >= 0 && i < 30/size) {
-            for (j=0; j<NY; j++) {
-                for (k=0; k<NX; k++) {
-                    a = 28 / size;
-                    b = 20 / size;
-                    left = pow(k-64, 2) / pow(a, 2);
-                    right = pow(j-64, 2) / pow(b, 2);
-                    if (left + right <= 1.0) {
-                        img[j][k] = '255';
-                    }
-                }
-            }
-        }
-
-        // File write
-        for (j=0; j<NY; j++) {
-            fwrite(img[j], NX, sizeof(char), fp);
-        }
-
+    for (i=0; i<NY; i++) {
+        fwrite(img[i], NX, sizeof(char), fp);
     }
 
     fclose(fp);
