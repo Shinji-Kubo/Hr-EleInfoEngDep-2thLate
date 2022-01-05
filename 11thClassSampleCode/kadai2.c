dc581ff148f8ebb763a4c7e90290fe0a97ab0a46
Shinji Kubo <s.2001kubo@outlook.jp>
2022-01-05 20:59:34 +0900

    11thClass

--- 11thClassSampleCode/kadai2.c
+++ 11thClassSampleCode/kadai2.c
@@ -14,12 +14,6 @@ int main(void) {
 
     size = 0.25;
 
-    fp = fopen(fn, "wb");
-    if (fp==NULL) {
-        printf("File open error \n");
-        exit(1);
-    }
-
     for (i=0; i<NY; i++) {
         for (j=0; j<NX; j++) {
             img[i][j] = '0';
@@ -30,10 +24,17 @@ int main(void) {
         for (j=0; j<NX; j++) {
             left  = pow(j-64, 2) / pow(28/size, 2);
             right = pow(i-64, 2) / pow(20/size, 2);
+            printf("%lf : %lf\n", left, right);
             if (left + right <= 1.0) {
                 img[i][j] = '120';
             }
         }
+    }
+
+    fp = fopen(fn, "wb");
+    if (fp == NULL) {
+        printf("File open error \n");
+        exit(1);
     }
 
     for (i=0; i<NY; i++) {
